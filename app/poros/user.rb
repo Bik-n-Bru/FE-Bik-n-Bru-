8c78eabccc3a0076939dab7a0c766d291541a32e
Gabe Nunez <108249540+MisterJackpots@users.noreply.github.com>
2023-01-05 15:36:33 -0700

    Update user.rb
    City and state not being used from Strava, information will be entered by user and stored on the back end.

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -2,9 +2,7 @@ class User
   attr_reader :id,
               :athlete_id,
               :username,
-              :token,
-              :city,
-              :state
+              :token
   def initialize(user_data)
     @id = user_data[:data][:id]
     @athlete_id = user_data[:data][:attributes][:athlete_id]

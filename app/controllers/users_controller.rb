c7e2bab3a0b3206581ac2bcfe3bebede306c536b
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-05 18:23:47 -0700

    Add dashboard and logout routes, dashboard controller, and logout action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def login
     
   end
+
+  def logout
+    reset_session
+    redirect_to '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'users#login'
-  
+  get '/logout', to: "users#logout"
+
   get '/auth/strava/callback', to: 'sessions#create'
+
+  get '/dashboard', to: 'dashboard#index'
 end

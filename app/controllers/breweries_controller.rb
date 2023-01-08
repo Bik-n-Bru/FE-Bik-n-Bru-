a2be392985b9b374027264be43fd46190adab105
Annie Pulzone <ajpulzone@gmail.com>
2023-01-07 20:27:15 -0800

    Add view for breweries index page, brewery index action in controller and testing for the view if a user has a city and state already filled out on their profile

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,15 +1,7 @@
 class BreweriesController < ApplicationController
-  # before_action :require_user
 
   def index
-    binding.pry
-    # @user = User.find[params(:user_id)]
-
-    @breweries = BreweryFacade.user_location_breweries(params[:user_id])
+    @user = UsersFacade.user_detail(session[:user_id])
+    @breweries = BreweryFacade.user_location_breweries(session[:user_id])
   end
-
-  # private
-  #   def require_user
-  #     render file: "/public/404" unless current_user?
-  #   end
 end

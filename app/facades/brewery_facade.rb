03cd8e27115f1aacfbbc50f0414387c4f2a1ad33
Annie Pulzone <ajpulzone@gmail.com>
2023-01-07 19:05:40 -0800

    Added Brewery facade and associated testing

--- app/facades/brewery_facade.rb
+++ app/facades/brewery_facade.rb
@@ -1,7 +1,9 @@
 class BreweryFacade
 
   def self.user_location_breweries(user_id)
-    data = BEService.breweries_by_user_location[:data]
-    data.map { |brewery| Brewery.new(brewery) }
-  end
+    results = BEService.breweries_by_user_location(user_id)
+    results[:data].map do |brewery|
+      Brewery.new(brewery)
+    end 
+  end 
 end

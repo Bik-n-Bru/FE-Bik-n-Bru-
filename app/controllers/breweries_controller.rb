43e97c612720ddc4bc0366ccd5f438692e66f1ff
Annie Pulzone <ajpulzone@gmail.com>
2023-01-08 15:01:35 -0800

    update controller to make notes on how to use open_struct and to refactor using memoization

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,7 +1,9 @@
 class BreweriesController < ApplicationController
 
   def index
-    @user = UsersFacade.user_detail(session[:user_id])
-    @breweries = BreweryFacade.user_location_breweries(session[:user_id])
+    #will refactor to use memoization when Rich merges his branch.
+    # @open_struct = OpenStruct.new(user, breweries)
+    user = UsersFacade.user_detail(session[:user_id])
+    breweries = BreweryFacade.user_location_breweries(session[:user_id])
   end
 end

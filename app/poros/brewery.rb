f3a5acf7f24c83d5e60f8f071e65eeca487c0f77
Annie Pulzone <ajpulzone@gmail.com>
2023-01-07 19:13:55 -0800

    update brewery poros and testing

--- app/poros/brewery.rb
+++ app/poros/brewery.rb
@@ -1,41 +1,15 @@
 class Brewery
-  attr_reader :id, :name, :address, :phone_number, :website, :type
+  attr_reader :id, :name, :street_address, :city, :state, :zip_code, :phone_number, :website, :type
   
   def initialize(brewery_data)
     @id = brewery_data[:id]
     @name = brewery_data[:attributes][:name]
-    @address = brewery_data[:attributes][:street], 
-               brewery_data[:attributes][:city], 
-               brewery_data[:attributes][:state],
-               brewery_data[:attributes][:postal_code]
+    @street_address = brewery_data[:attributes][:street_address]
+    @city = brewery_data[:attributes][:city]
+    @state = brewery_data[:attributes][:state]
+    @zip_code = brewery_data[:attributes][:zipcode]
     @phone_number = brewery_data[:attributes][:phone]
     @website = brewery_data[:attributes][:website_url]
-    @type = brewery_data[:attributes][:brewery_type]
+    @type = brewery_data[:type]
   end
-
-  def phone_number_conversion(phone_number)
-    phone_number.insert(3, "-").insert(6, "-")
-  end
-
 end
-
-
-# expect(first_brewery).to eq({
-    #     :id=>"10-barrel-brewing-co-denver-denver",
-    #     :name=>"10 Barrel Brewing Co - Denver",
-    #     :brewery_type=>"large",
-    #     :street=>"2620 Walnut St",
-    #     :address_2=>nil,
-    #     :address_3=>nil,
-    #     :city=>"Denver",
-    #     :state=>"Colorado",
-    #     :county_province=>nil,
-    #     :postal_code=>"80205-2231",
-    #     :country=>"United States",
-    #     :longitude=>"-104.9853655",
-    #     :latitude=>"39.7592508",
-    #     :phone=>"7205738992",
-    #     :website_url=>nil,
-    #     :updated_at=>"2023-01-04T04:46:02.393Z",
-    #     :created_at=>"2023-01-04T04:46:02.393Z"})
-    # end
--- spec/poros/breweries_spec.rb
+++ spec/poros/breweries_spec.rb
@@ -2,32 +2,23 @@ require "rails_helper"
 
 RSpec.describe "Brewery Poros" do
 
-  xdescribe "attributes" do
+  describe "attributes" do
     it "exists and has attributes" do
-      attributes = {id: "1", attributes: {name: "Deschutes Brewery", address: "1222 NE 11th St, Bend, Or 97701", phone_number_: "123-456-7890" , website: "https://www.deschutesbrewery.com/", type: "micro" }}
+      attributes = {id: "10-barrel-brewing-co-bend-1", type: "micro", attributes: {name: "10 Barrel Brewing Co", street_address: "62970 18th St", city: "Bend", state: "Oregon", zipcode: "97701-9847", phone: "5415851007" , website_url: "https://www.deschutesbrewery.com/" }}
       
       brewery = Brewery.new(attributes)
 
       expect(brewery).to be_an_instance_of(Brewery)
-      expect(brewery.id).to eq("1")
-      expect(brewery.name).to eq("Deschutes Brewery")
-      expect(brewery.address).to eq("1222 NE 11th St, Bend, Or 97701")
-      expect(brewery.phone_number).to eq("123-456-7890")
+      expect(brewery.id).to eq("10-barrel-brewing-co-bend-1")
+      expect(brewery.name).to eq("10 Barrel Brewing Co")
+      expect(brewery.street_address).to eq("62970 18th St")
+      expect(brewery.city).to eq("Bend")
+      expect(brewery.state).to eq("Oregon")
+      expect(brewery.zip_code).to eq("97701-9847")
+      expect(brewery.phone_number).to eq("5415851007")
       expect(brewery.website).to eq("https://www.deschutesbrewery.com/")
       expect(brewery.type).to eq("micro")
     end
   end 
-
-  describe "class methods" do
-    it "#phone_number_conversion" do
-      attributes = {id: "10-barrel-brewing-co-denver-denver", name: "10 Barrel Brewing Co - Denver", street: "2620 Walnut St", city: "Denver", state: "Colorado", postal_code: "80205-2231", phone: "7205738992" , website_url: "https://www.deschutesbrewery.com/", brewery_type: "large" }
-      brewery = Brewery.new(attributes)
-      binding.pry
-      expect(brewery.phone_number.phone_number_conversion(phone_number)).to eq("123-456-7890")
-      # expect(phone_number_2.phone_number_conversion(phone_number_1)).to eq("223-334-4456")
-    end
-  end
-
-
 end
 

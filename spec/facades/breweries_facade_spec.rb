3272264d2f3536adfc110ce938cc23f72e271af8
Annie Pulzone <ajpulzone@gmail.com>
2023-01-07 19:14:26 -0800

    update brewery facade and associated testing

--- spec/facades/breweries_facade_spec.rb
+++ spec/facades/breweries_facade_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe BreweryFacade do
         breweries = BreweryFacade.user_location_breweries(user_id)
 
         expect(breweries).to be_an_instance_of(Array)
+        expect(breweries.count).to eq(24)
         expect(breweries[0].id).to eq("10-barrel-brewing-co-bend-1")
         expect(breweries[0].name).to eq("10 Barrel Brewing Co")
         expect(breweries[0].street_address).to eq("62970 18th St")
@@ -16,7 +17,8 @@ RSpec.describe BreweryFacade do
         expect(breweries[0].zip_code).to eq("97701-9847")
         expect(breweries[0].phone_number).to eq("5415851007")
         expect(breweries[1].id).to eq("10-barrel-brewing-co-bend-2")
-        
+
+        expect(breweries.count).to_not eq(50)
         expect(breweries[2].id).to_not eq("10-barrel-brewing-co-bend-1")
       end
     end 

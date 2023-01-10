8f581ceb53ecfa981a6eedbd1258a0604104ca31
Rich Kaht <adamselene1776@protonmail.com>
2023-01-09 19:13:31 -0700

    Fix: Redirect user to login page if they cancel OAuth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
   def create
     user_data = StravaService.authorize_user_account(params[:code])
+    if params[:error] == 'access_denied'
+      redirect_to '/'
+    else
+      serialized_user_data = StravaAuthSerializer.serialize(user_data)
+      user_parsed_json = BEService.login_user(serialized_user_data)
 
-    serialized_user_data = StravaAuthSerializer.serialize(user_data)
-
-    user_parsed_json = BEService.login_user(serialized_user_data)
-
-    @user = User.new(user_parsed_json)
-    session[:user_id] = @user.id
-    redirect_to '/dashboard'
+      @user = User.new(user_parsed_json)
+      session[:user_id] = @user.id
+      redirect_to '/dashboard'
+    end
   end
 end
